a3f67bca05298aa99df810462bb78a25d692da48
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-22 13:30:04 +0530

    File structure decided for the attendence

---
+++ Attendance/index.php
@@ -0,0 +1,12 @@
+<?php   
+    $title='Index';
+    require 'includes/header.php';
+?>
+
+<!-- Body -->
+
+<h1>Hello, world!</h1>
+
+<!-- Ending of body -->
+
+<?php require 'includes/footer.php'; ?>
