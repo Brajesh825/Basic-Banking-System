4e231ed548f7bc71d7f584713e76160f14714386
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-27 14:37:31 +0530

    Created Home controller and view and added bootstrap

---
+++ ci/application/views/home.php
@@ -0,0 +1,20 @@
+<?php        
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>           
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ ci/application/views/inc/header.php
@@ -0,0 +1,14 @@
+<?php        
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>       
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
