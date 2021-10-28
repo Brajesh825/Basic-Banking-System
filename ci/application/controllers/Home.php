def4b7c347c90b70418316e954ec335bed4a5671
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-28 19:11:02 +0530

    Created Registration Form

--- ci/application/controllers/Home.php
+++ ci/application/controllers/Home.php
@@ -11,6 +11,44 @@
            $this->load->view('inc/footer');
         }
     
+        public function register()
+        {
+           $this->load->view('inc/header');            
+           $this->load->view('register');
+           $this->load->view('inc/footer');
+        }
+
+        public function login_process(){
+            if( $this->input->post('u_login')){
+                $u_name = $this->input->post('u_name');
+                $u_pass = md5($this->input->post('u_password'));
+
+                $user_data=array(
+                    'u_name' => $u_name,
+                    'u_pass' => $u_pass
+                );
+                print_r ($user_data);
+            }else{
+                redirect('home','refresh');
+            }
+        }
+
+        public function register_process(){
+            if( $this->input->post('u_reg')){
+                $u_email = $this->input->post('u_email'); 
+                $u_name = $this->input->post('u_name');
+                $u_pass = md5($this->input->post('u_password'));
+
+                $user_data=array(
+                    'u_email' => $u_email,
+                    'u_name' => $u_name,
+                    'u_pass' => $u_pass
+                );
+                print_r ($user_data);
+            }else{
+                redirect('home','refresh');
+            }
+        }
     }
     
     /* End of file Home.php */
--- ci/application/views/register.php
+++ ci/application/views/register.php
@@ -6,9 +6,12 @@
     <div class="row">
         <div class="col-lg-4 col-md-4 col-lg-oush-4 col-md-push-4">
             <div class="panel panel-default" style="margin-top: 50px;">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading">Register</div>
                 <div class="panel-body">
-                    <?php echo form_open('home/login_process'); ?>
+                    <?php echo form_open('home/register_process'); ?>
+                    <div class="form-group">
+                        <input type="email" name="u_email" placeholder="Email" required>
+                    </div>
                     <div class="form-group">
                         <input type="text" name="u_name" placeholder="Username" required>
                     </div>
@@ -16,8 +19,8 @@
                         <input type="password" name="u_password" placeholder="Password" required>
                     </div>
                     <div class="form-group">
-                        <input type="submit" name="u_login" value="Login as Admin" class="btn btn-success btn-sm">
-                        <a href="<?php echo site_url('home/register')?>" class="btn btn-warning btn-sm">Register</a>
+                        <input type="submit" name="u_reg" value="Register" class="btn btn-success btn-sm">
+                        <a href="<?php echo site_url('home')?>" class="btn btn-warning btn-sm">Login as Admin</a>
                     </div>
                     <?php echo form_close(); ?>
                 </div>
