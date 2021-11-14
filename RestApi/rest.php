38b7ff5788a296762b43638494ec2c2c170a5fbc
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-14 15:41:11 +0530

    Rest Api Using Php

---
+++ RestApi/rest.php
@@ -0,0 +1,33 @@
+<?php 
+    class Rest{
+        public function __construct()
+        {
+            
+        }
+
+        public function validateRequest()
+        {
+
+        }
+
+        public function processApi()
+        {
+
+        }
+
+        public function validateParameters($fieldName,$value,$dataType,$required)
+        {
+
+        }
+
+        public function ThrowError($code,$messege)
+        {
+
+        }
+
+        public function returnResponse()
+        {
+
+
+        }
+?>
